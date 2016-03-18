fa9f2910fbf73d4184f85450d3550943217122a2
unknown <l1111j@live.com>
2016-03-18 16:55:04 +0800

    em

--- src/com/szse/po/service/process/ProcessAll.java
+++ src/com/szse/po/service/process/ProcessAll.java
@@ -7,6 +7,7 @@ public class ProcessAll {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		//Spider spi=new Spider();
 		//spi.StartSpider();
 		//Classifier.classify();
@@ -14,5 +15,7 @@ public class ProcessAll {
 		
 		
 	}
+	
+	
 
 }
