f9b4c9326e08c04cef466660ae095f7af4f32169
unknown <l1111j@live.com>
2016-03-23 16:59:34 +0800

    Error adding vectors

--- src/com/szse/po/service/process/ProcessAll.java
+++ src/com/szse/po/service/process/ProcessAll.java
@@ -1,5 +1,6 @@
 package com.szse.po.service.process;
 import com.szse.po.service.classification.Classifier;
+import com.szse.po.service.tools.*;
 import com.szse.po.service.spider.*;
 import com.szse.po.sentiment.analysis.*;
 import com.szse.po.sentiment.analysis.process.SentimentAnalysis;
@@ -8,10 +9,22 @@ public class ProcessAll {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Spider the Newest Data.
 		Spider spi=new Spider();
 		spi.StartSpider();
+		
+		//Classify and Get the Stock Code.
 		Classifier.classify();
+		
+		//Analyze the Sentiment.
 		SentimentAnalysis.analyseAll();
+		
+		//Get the Keywords.
+		SaveTools.saveKeywords();
+		
+		//LDA Topic Generation.
+		
+		
 		
 		
 	}
--- src/com/szse/po/service/tools/SaveTools.java
+++ src/com/szse/po/service/tools/SaveTools.java
@@ -2,6 +2,7 @@ package com.szse.po.service.tools;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -16,7 +17,8 @@ import com.szse.po.service.classification.Data;
 public class SaveTools {
 	public static void main(String[] args)
 	{
-		saveKeywords();
+		//saveKeywords();
+		saveVector();
 	}
 
 	public static void saveKeywords()//Save NLPIR output Keywords to 'miningresult'
@@ -40,6 +42,46 @@ public class SaveTools {
 				mr.setKeywords(NlpirDemo.getKeywords(text));
 				mr.setUpdatetime(time);
 				mrd.save(mr);
+			}
+			
+			
+			
+		}
+	}
+	
+	public static void saveVector()//Save Content Vector 'textdata'
+	{
+		String argu = "D:\\MyEclipse2015WorkSpace\\JnaTest_NLPIR\\ICTCLAS2015\\";
+		// String system_charset = "GBK";//GBK----0
+		NlpirDemo nd=new NlpirDemo(argu);
+		Textdata td=new Textdata();
+		TextdataDAO tdd=new TextdataDAO();
+		List<Textdata> l=tdd.findAll();
+		Date date=new Date();
+		DateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String time=format.format(date);
+		for(int i=0;i<l.size();i++)
+		{
+			td=l.get(i);
+			
+			String text=td.getTitle()+" "+td.getContent();
+			//System.out.println(text);
+			if(td.getVector()!=null&&!text.equals(""))
+			{
+				ArrayList<String> list=nd.parseWordReturnList(text);
+				StringBuffer sb=new StringBuffer();
+				for(int k=0;k<list.size();k++)
+				{
+					sb.append(list.get(k));
+					if(k<list.size()-1)
+					{
+						sb.append(" ");
+					}
+				}
+				System.out.println(new String(sb));
+				td.setVector(new String(sb));
+				tdd.save(td);
+				
 			}
 			
 			
